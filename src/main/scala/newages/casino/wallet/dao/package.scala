377c1654d3cb9803c8dd935e74a920b903dd23da
Simon Rubanenko <rubanenkos@gmail.com>
2021-03-25 17:08:05 +0300

    refactoring

--- src/main/scala/newages/casino/wallet/dao/package.scala
+++
@@ -1,9 +0,0 @@
-package newages.casino.wallet
-
-import newages.casino.wallet.model.AccountId
-import newages.casino.wallet.model.AccountType.AccountType
-
-package object dao {
-  final case class Account(id: AccountId, amount: BigDecimal, accountType: AccountType)
-
-}
--- src/main/scala/newages/casino/wallet/persistence/TransactionActor.scala
+++
@@ -1,7 +0,0 @@
-package newages.casino.wallet.persistence
-
-import newages.casino.wallet.model.TransactionId
-
-object TransactionActor {
-  final case class TransactionState(transactionId: TransactionId) extends ProtobufSerializable
-}
